8496f90ca32774d7f7549dcf12e1bd4f92608143
dimihris <hristo_dimitrov@flutterint.com>
2026-09-23 15:02:55 +0300

    initial commit

---
+++ src/test/java/com/dimihris/gatewayserver/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dimihris.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
